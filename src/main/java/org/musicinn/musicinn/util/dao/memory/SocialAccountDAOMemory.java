38af7c6db375ef0550ce40d26fe0041579893437
Andrea Tosi <andreatosinho@gmail.com>
2026-01-30 11:23:23 +0100

    SocialAccountDAOMemory.java deleted

--- src/main/java/org/musicinn/musicinn/util/dao/memory/SocialAccountDAOMemory.java
+++
@@ -1,9 +0,0 @@
-package org.musicinn.musicinn.util.dao.memory;
-
-import org.musicinn.musicinn.model.SocialAccount;
-
-public class SocialAccountDAOMemory {
-    public void create(SocialAccount socialAccount, String username) {
-        System.out.println("social account (" + socialAccount.getSocial() + ") di " + username + " creato");
-    }
-}
