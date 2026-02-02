6f67e6720d78a99b43aaec1b829c38ccbd0c8489
Andrea Tosi <andreatosinho@gmail.com>
2026-02-03 00:56:58 +0100

    code smells fixed

--- src/main/java/org/musicinn/musicinn/util/EmailSender.java
+++ src/main/java/org/musicinn/musicinn/util/EmailSender.java
@@ -33,7 +33,7 @@ public class EmailSender {
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
             Response response = sg.api(request);
-            LOGGER.log(Level.FINE, String.valueOf(response.getStatusCode()));
+            LOGGER.log(Level.FINE, "Status code: {0}", response.getStatusCode());
             LOGGER.fine(response.getBody());
         } catch (IOException e) {
             LOGGER.fine(e.getMessage());
